cff1d16d0730ecb5717ea6c35b5ccee5e7ebb4c6
chacham <ccm0702@naver.com>
2023-04-30 14:16:08 +0900

    Init spring boot project

---
+++ src/main/kotlin/me/chacham/dongbaek/DongbaekApplication.kt
@@ -0,0 +1,11 @@
+package me.chacham.dongbaek
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DongbaekApplication
+
+fun main(args: Array<String>) {
+	runApplication<DongbaekApplication>(*args)
+}
---
+++ src/test/kotlin/me/chacham/dongbaek/DongbaekApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.chacham.dongbaek
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DongbaekApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
